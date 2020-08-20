92c6ab5cc02ac2a89516d74a318c2c7176ffa47a
Andi Sultan <andi.sultan@protonmail.com>
2020-08-20 10:55:02 +0800

    Revert "Update udemy-set-video-height.js"
    This reverts commit 6e57e3b7c8995bfe3318e6f01c89eae7a3b96812.

--- udemy-set-video-height.js
+++ udemy-set-video-height.js
@@ -1,5 +1,5 @@
 document
   .querySelectorAll(".curriculum-item-view--scaled-height-limiter--1j3Pp")
   .forEach(function (a) {
-    a.setAttribute("style", "max-height: 95vh;");
+    a.setAttribute("style", "max-height:calc(100vh - 100px);");
   });

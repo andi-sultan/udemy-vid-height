3084090bb627d58540f2f33ffa74179efb459d71
Andi Sultan <andi.sultan@protonmail.com>
2020-08-20 10:45:59 +0800

    Initial commit

---
+++ udemy-set-video-height.js
@@ -0,0 +1,5 @@
+document
+  .querySelectorAll(".curriculum-item-view--scaled-height-limiter--1j3Pp")
+  .forEach(function (a) {
+    a.setAttribute("style", "max-height:calc(100vh - 100px);");
+  });

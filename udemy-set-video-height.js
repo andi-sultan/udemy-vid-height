e8cff61143d4d0251e4c0bf4680f064b5ff69699
Andi Sultan <andisultan317@gmail.com>
2022-10-03 23:45:14 +0800

    Update to manifest version 3

--- udemy-set-video-height.js
+++ udemy-set-video-height.js
@@ -1,5 +1,12 @@
-document
-  .querySelectorAll(".curriculum-item-view--scaled-height-limiter--1j3Pp")
-  .forEach(function (a) {
-    a.setAttribute("style", "max-height:calc(100vh - 100px);");
-  });
+(() => {
+  // To make sure the script below is executed after the element loaded
+  for (let i = 1; i <= 10; i++) {
+    setTimeout(function () {
+      document
+        .querySelectorAll(".curriculum-item-view--scaled-height-limiter--1j3Pp")
+        .forEach(function (a) {
+          a.setAttribute("style", "max-height:calc(100vh);");
+        });
+    }, 1000 * i);
+  }
+})();
